7347f3f9845a2fc560956ae0c32849ec06741f2c
hirabarufumitaka <114893728+hirabarufumitaka@users.noreply.github.com>
2023-05-08 01:09:02 +0900

    犬のアスキーアートを追加

---
+++ cmd/dog.go
@@ -0,0 +1,47 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// dogCmd represents the dog command
+var dogCmd = &cobra.Command{
+	Use:   "dog",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("dog called")
+		// aa/dog.txt ファイルを読み込んで表示する
+		b, err := os.ReadFile("aa/dog.txt")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(string(b))
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(dogCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// dogCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// dogCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
